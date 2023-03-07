f2643675da8b705cccb19cf56e687052aaa1d532
devel12 <gzlakshya@gmail.com>
2023-03-07 12:34:19 +0530

    ui minor

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ if "past" not in st.session_state:
 
 
 def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+    input_text = st.text_input("You: ", "type your question here...", key="input")
     print(f'Inside get_text, input_text = {input_text}')
     return input_text
 
