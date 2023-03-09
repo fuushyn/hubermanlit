712d497d21291e94e80ce25bbdd899c996ddf41e
devel12 <gzlakshya@gmail.com>
2023-03-09 20:15:35 +0530

    changed refine to stuff

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ import pinecone
 
 
 basic_llm = OpenAI(temperature=0)
-chat_llm = OpenAIChat(model_name="gpt-3.5-turbo", max_tokens=1000)
+chat_llm = OpenAIChat(model_name="gpt-3.5-turbo")
 
 base_embeddings = OpenAIEmbeddings()
 hypothetical_embeddings = HypotheticalDocumentEmbedder.from_llm(chat_llm, base_embeddings, "web_search")
@@ -50,55 +50,64 @@ pinecone.init(
 index_name = "askhuberman"
 
 
-refine_template = (
-    "The original question is as follows: {question}\n"
-    "We have provided an existing answer, including sources (just the ones given in the metadata of the documents, don't make up your own sources): {existing_answer}\n"
-    "We have the opportunity to refine the existing answer"
-    "(only if needed) with some more context below.\n"
-    "------------\n"
-    "{context_str}\n"
-    "------------\n"
-    "Given the new context, add to the original answer to better "
-    "answer the question"
-    "If you do update it, please update the sources as well. "
-    "If the context isn't useful, print the original answer."
-    "Use only the context passed to you and original answer to give answers. Don't use any other information. Don't make up stuff on your own. If you don't have data, it's ok to just answer based on the given context and answer"
-    "The final answer should incorporate information from the original answer and the new context, but "
-    "don't make use of phrases like 'additional context', 'original answer','new context', 'old answer' \n "
-    "don't make use of phrases like 'additional context', 'original answer','new context', 'old answer'\n "
-    "Do not use external sources at any cost, use only the context provided.\n"
-    "Include the sources in the answer\n"
-    "Please update the answer only if the new information is relevant to the question.\n"
-    "Try to be concise and brief"
-    "Format the answer so that it's easy to skim"
-)
-refine_prompt = PromptTemplate(
-    input_variables=["question", "existing_answer", "context_str"],
-    template=refine_template,
-)
+# refine_template = (
+#     "The original question is as follows: {question}\n"
+#     "We have provided an existing answer, including sources (just the ones given in the metadata of the documents, don't make up your own sources): {existing_answer}\n"
+#     "We have the opportunity to refine the existing answer"
+#     "(only if needed) with some more context below.\n"
+#     "------------\n"
+#     "{context_str}\n"
+#     "------------\n"
+#     "Given the new context, add to the original answer to better "
+#     "answer the question"
+#     "If you do update it, please update the sources as well. "
+#     "If the context isn't useful, print the original answer."
+#     "Use only the context passed to you and original answer to give answers. Don't use any other information. Don't make up stuff on your own. If you don't have data, it's ok to just answer based on the given context and answer"
+#     "The final answer should incorporate information from the original answer and the new context, but "
+#     "don't make use of phrases like 'additional context', 'original answer','new context', 'old answer' \n "
+#     "don't make use of phrases like 'additional context', 'original answer','new context', 'old answer'\n "
+#     "Do not use external sources at any cost, use only the context provided.\n"
+#     "Include the sources in the answer\n"
+#     "Please update the answer only if the new information is relevant to the question.\n"
+#     "Try to be concise and brief"
+#     "Format the answer so that it's easy to skim"
+# )
+# refine_prompt = PromptTemplate(
+#     input_variables=["question", "existing_answer", "context_str"],
+#     template=refine_template,
+# )
 
 
-question_template = (
-    "Context information is below. \n"
-    "---------------------\n"
-    "{context_str}"
-    "\n---------------------\n"
-    "Given the context information and not prior knowledge, "
-    "answer the question: {question}\n"
-)
+# question_template = (
+#     "Context information is below. \n"
+#     "---------------------\n"
+#     "{context_str}"
+#     "\n---------------------\n"
+#     "Given the context information and not prior knowledge, "
+#     "answer the question: {question}\n"
+# )
 
-question_prompt = PromptTemplate(
-    input_variables=["context_str", "question"], template=question_template
-)
+# question_prompt = PromptTemplate(
+#     input_variables=["context_str", "question"], template=question_template
+# )
 
 
 pinecone.list_indexes()
 index = pinecone.Index(index_name)
-docsearch = Pinecone(index, hypothetical_embeddings.embed_query, 'text')
+# docsearch = Pinecone(index, hypothetical_embeddings.embed_query, 'text')
 
-doc_chain_refine = load_qa_with_sources_chain(chat_llm, chain_type="refine",question_prompt=question_prompt, refine_prompt=refine_prompt)
+
+
+
+# doc_chain_refine = load_qa_with_sources_chain(chat_llm, chain_type="refine",question_prompt=question_prompt, refine_prompt=refine_prompt)
 
 question_generator = LLMChain(llm=basic_llm, prompt=CONDENSE_QUESTION_PROMPT)
+
+docsearch_base = Pinecone(index, base_embeddings.embed_query, 'text')
+
+doc_chain_refine_base_chat = load_qa_with_sources_chain(chat_llm, chain_type="stuff")
+
+qa_base1 = ChatVectorDBChain(vectorstore=docsearch_base, combine_docs_chain=doc_chain_refine_base_chat, question_generator=question_generator, return_source_documents=True, top_k_docs_for_context=7)
 
 
 # query = "How to lose fat? Answer in bullet points"
@@ -106,15 +115,7 @@ question_generator = LLMChain(llm=basic_llm, prompt=CONDENSE_QUESTION_PROMPT)
 
 # print(result['answer'])
 
-def load_chain():
-    """Logic for loading the chain you want to use should go here."""
-
-
-    chain = ChatVectorDBChain(vectorstore=docsearch, combine_docs_chain=doc_chain_refine, question_generator=question_generator, return_source_documents=True, top_k_docs_for_context=10)
-
-    return chain
-
-chain = load_chain()
+chain = qa_base1
 
 # From here down is all the StreamLit UI.
 st.set_page_config(page_title="Ask Huberman Lab", page_icon=":robot:")
